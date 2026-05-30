414313d7421201904b75a7aa2287594a1ee9f045
productiondp <production.dp30@gmail.com>
2026-05-30 19:41:12 +0530

    chore: remove Creator Profile link from sidebar

--- src/components/layout/DesktopSidebar.tsx
+++ src/components/layout/DesktopSidebar.tsx
@@ -51,7 +51,6 @@ export default function DesktopSidebar() {
         { label: "Marketplace", icon: ShoppingBag, href: "/marketplace" },
         { label: "Advisors", icon: Target, href: "/advisors" },
         { label: "Creator Library", icon: Compass, href: "/creators" },
-        { label: "Creator Profile", icon: User, href: "/creators/profile" },
         { label: "Meetups", icon: LayoutGrid, href: "/meetup" },
       ]
     },
